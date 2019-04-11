e095cf4c6ffcdfdc8b2723b3265903ae103363e5
Mark Canlas <github@htmlism.com>
2019-04-11 01:20:49 -0400

    print as table

--- src/main/scala/com/htmlism/marvelstrikeforce/HitList.scala
+++ src/main/scala/com/htmlism/marvelstrikeforce/HitList.scala
@@ -7,6 +7,16 @@ object HitList extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
     CharacterOracle[IO]
       .characters
-      .map(_.foreach(println))
+      .map(toTable)
+      .map(TablePrinter.printTbl)
       .as(ExitCode.Success)
+
+  private def toTable(xs: List[Character]) = {
+    val cells = ("Name" :: xs.map(_.name).sorted)
+      .map(s => Cell(List(s)))
+
+    Table
+      .empty
+      .addColumn(cells)
+  }
 }
---
+++ src/main/scala/com/htmlism/marvelstrikeforce/Table.scala
@@ -0,0 +1,91 @@
+package com.htmlism.marvelstrikeforce
+
+object Table {
+  val empty: Table = Table(Nil, 0, 0)
+}
+
+case class Table(cells: List[Cell], height: Int, width: Int) {
+  def cellAt(x: Int, y: Int): Cell =
+    cells((y * width) + x)
+
+  def row(n: Int): List[Cell] =
+    cells.slice(n * width, n * width + width)
+
+  def addColumn(col: List[Cell]): Table = {
+    val newWidth = width + 1
+    val newHeight =
+      if (height == 0)
+        col.length
+      else
+        height
+
+    val cellsWithNewColumn =
+      (0 until newHeight).toList
+        .flatMap(h => row(h) :+ col(h))
+
+    Table(cellsWithNewColumn, newHeight, newWidth)
+  }
+
+  def columnWidth(w: Int): Int = {
+    (0 until height).toList
+      .map { h =>
+        cellAt(w, h)
+      }
+      .map(_.maximumWidth)
+      .max
+  }
+
+  def rowHeight(h: Int): Int = {
+    (0 until width).toList
+      .map { w =>
+        cellAt(w, h)
+      }
+      .map(_.height)
+      .max
+  }
+}
+
+object Cell {
+  val empty: Cell = Cell(Nil)
+}
+
+case class Cell(lines: List[String]) {
+  def maximumWidth: Int =
+    lines
+      .map(_.length)
+      .max
+
+  def height: Int =
+    lines.length
+
+  def line(n: Int): String =
+    lines(n)
+}
+
+object TablePrinter {
+  def printTbl(tbl: Table): Unit = {
+    val dashWidth =
+      (0 until tbl.width)
+        .map(tbl.columnWidth)
+        .map(_ + 3) // left pad, right pad, side line
+        .sum + 1 // last sideline
+
+    println("-" * dashWidth)
+
+    for (h <- 0 until tbl.height) {
+      for (rh <- 0 until tbl.rowHeight(h)) {
+        for (w <- 0 until tbl.width) {
+          val cell  = tbl.cellAt(w, h)
+          val line  = cell.line(rh)
+          val width = tbl.columnWidth(w)
+
+          print("|")
+          printf(s" %-${width}s ", line)
+        }
+        println("|")
+      }
+
+      println("-" * dashWidth)
+    }
+  }
+}
