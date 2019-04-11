3cd45d7992c5d50321c91847c2b0de4ee983bce2
Mark Canlas <github@htmlism.com>
2019-04-11 02:55:10 -0400

    complete roster

--- src/main/scala/com/htmlism/marvelstrikeforce/HitList.scala
+++ src/main/scala/com/htmlism/marvelstrikeforce/HitList.scala
@@ -8,15 +8,55 @@ object HitList extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
     for {
       cs <- CharacterOracle[IO].characters
-      _ <- IO { cs |> toTable |> TablePrinter.printTbl }
+      ts <- new YamlLoader[IO].traits
+      _ <- IO { cs |> toTable(ts) |> TablePrinter.printTbl }
     } yield ExitCode.Success
 
-  private def toTable(xs: List[Character]) = {
-    val cells = ("Name" :: xs.map(_.name).sorted)
+  private def toTable(ts: Map[String, List[Trait]])(xs: List[Character]) = {
+    val sortedCharacters =
+      xs
+        .sortBy(_.name)
+
+    val names = ("Name" :: sortedCharacters.map(_.name))
       .map(s => Cell(List(s)))
+
+    val allegiance =
+      narrowTraits("Allegiance", sortedCharacters, ts)
+
+    val roles =
+      narrowTraits("Role", sortedCharacters, ts)
+
+    val origins =
+      narrowTraits("Origin", sortedCharacters, ts)
+
+    val jurisdiction =
+      narrowTraits("Jurisdiction", sortedCharacters, ts)
+
+    val teams =
+      narrowTraits("Team", sortedCharacters, ts)
 
     Table
       .empty
-      .addColumn(cells)
+      .addColumn(names)
+      .addColumn(allegiance)
+      .addColumn(jurisdiction)
+      .addColumn(origins)
+      .addColumn(roles)
+      .addColumn(teams)
+  }
+
+  def narrowTraits(s: String, characters: List[Character], traits: String Map List[Trait]): List[Cell] = {
+    val collide = traits(s)
+
+    val traitsForCharacters =
+      characters
+        .map { c =>
+          c.traits.filter(collide.toSet).map(_.s).mkString(", ")
+        }
+
+    val strings = s :: traitsForCharacters
+
+    strings
+      .map(s => Cell(List(s)))
   }
 }
