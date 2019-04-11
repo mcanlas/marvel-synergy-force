0d0c8bb2ff443726ed111d586a443834dd9af03d
Mark Canlas <github@htmlism.com>
2019-04-11 01:34:52 -0400

    as for comprehension

--- src/main/scala/com/htmlism/marvelstrikeforce/HitList.scala
+++ src/main/scala/com/htmlism/marvelstrikeforce/HitList.scala
@@ -2,14 +2,14 @@ package com.htmlism.marvelstrikeforce
 
 import cats.effect._
 import cats.implicits._
+import mouse.any._
 
 object HitList extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
-    CharacterOracle[IO]
-      .characters
-      .map(toTable)
-      .map(TablePrinter.printTbl)
-      .as(ExitCode.Success)
+    for {
+      cs <- CharacterOracle[IO].characters
+      _ <- IO { cs |> toTable |> TablePrinter.printTbl }
+    } yield ExitCode.Success
 
   private def toTable(xs: List[Character]) = {
     val cells = ("Name" :: xs.map(_.name).sorted)
