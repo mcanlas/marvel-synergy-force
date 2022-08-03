91bf695358449220ffd912de9d2d208b0291b0da
Mark Canlas <github@htmlism.com>
2022-08-02 22:31:44 -0400

    scalatest 3.2.13

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -14,7 +14,7 @@ object ProjectPlugin extends AutoPlugin {
         p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.3.14")
 
       def withTesting: Project =
-        p.settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.12" % "test")
+        p.settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.13" % "test")
 
       def withMouse: Project =
         p.settings(libraryDependencies += "org.typelevel" %% "mouse" % "0.26")
