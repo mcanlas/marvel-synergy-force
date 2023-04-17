5b368bad7a5a1a292fabaf1728617d265642406a
Mark Canlas <github@htmlism.com>
2023-04-17 03:59:54 -0400

    cats-effect 3.4.9

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -7,7 +7,7 @@ object ProjectPlugin extends AutoPlugin {
   object autoImport {
     implicit class ProjectOps(p: Project) {
       def withCatsEffect: Project =
-        p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.4.8")
+        p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.4.9")
 
       def withTesting: Project =
         p.settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.15" % "test")
