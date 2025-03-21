f8ac8223b459d47e5d16db92b8e45e093ae32875
Mark Canlas <github@htmlism.com>
2025-03-21 19:02:56 -0400

    scalatest 3.2.19 :large_blue_diamond:

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -10,7 +10,7 @@ object ProjectPlugin extends AutoPlugin {
         p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.6.0-RC2")
 
       def withTesting: Project =
-        p.settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.16" % "test")
+        p.settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.19" % "test")
 
       def withYaml: Project =
         p.settings(libraryDependencies += "io.circe" %% "circe-yaml" % "0.15.1")
